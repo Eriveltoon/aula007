fb2172c47a2d541fc9317889b31a4f218b901a80
ERIVELTON DOS SANTOS BARRETO <erivelton.sbarreto@CAS474696C148A.local>
2023-03-24 20:49:29 -0300

    Comentario de codigo

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 
 export default function NavBar(){
 
-    //Usa o contexto e acessa o conteudo que esta dentro do contexto(theme, toggleTheme)
+    //Usa o contexto e acessa o conteudo que esta dentro do contexto(theme, toggleTheme).
     const{theme, toggleTheme} = useContext(ThemeContex);
     return(
         <div className={theme}>
