782c7dd576d3ae1afcc76dffb63c0ab736160e3d
ERIVELTON DOS SANTOS BARRETO <erivelton.sbarreto@CAS474696C148A.local>
2023-03-24 21:28:52 -0300

    Criando Rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Routes, Route} from "react-router-dom";
 import ThemeProvider from "./ThemeProvider";
-import NavBar from "./NavBar";
+//import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import NotFound from "./NotFound";
 
 function App() {
+  //npm install react-router-dom (Biblioteca de Rotas)
   return (
     <ThemeProvider>
-      <NavBar/>
+      <Routes>
+        <Route path="/">
+
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="*" element={<NotFound/>}/>
+
+        </Route>
+      </Routes>
     </ThemeProvider>
   );
 }
